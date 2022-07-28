c33f0be24f3f64dbc903b495411106c15a72fdcf
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-07-28 11:24:28 +0300

    Create fastq_count_hisat2.sh

---
+++ fastq_count_hisat2.sh
@@ -0,0 +1,21 @@
+# put all fastq in the Directory 
+# the assumption is that the fastq files are r_1.fq.gz, f_2.fq.gz
+for i in $ls *1.fq.gz; 
+ do 
+ sam=${i//_1.fq.gz/.sam} 
+ fq2=${i//_1.fq.gz/_2.fq.gz} 
+ sbam=${i//_1.fq.gz/.sorted.bam} 
+ 
+ echo "===Alignment with hisat====" 
+ source /home/genwork1/anaconda3/etc/profile.d/conda.sh 
+ conda activate hisam 
+ hisat2 -x hg38 -1 ${i} -2 ${fq2} -S ${sam} -p 20 
+ 
+ echo "=======Bam works===========" 
+ source /home/genwork1/anaconda3/etc/profile.d/conda.sh 
+ conda activate sam 
+ samtools view -@ 20 -b ${sam} | samtools sort -@ 20 -O bam -o ${sbam} 
+ samtools index ${sbam} 
+ rm ${sam} 
+ echo "====the bam step is done========" 
+ done 
