e7da4aae429cb06751cae74207d9f6300c7c60d9
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-07-28 11:25:26 +0300

    Update fastq_count_hisat2.sh

--- fastq_count_hisat2.sh
+++ fastq_count_hisat2.sh
@@ -8,12 +8,10 @@ for i in $ls *1.fq.gz;
  
  echo "===Alignment with hisat====" 
  source /home/genwork1/anaconda3/etc/profile.d/conda.sh 
- conda activate hisam 
+ conda activate refhg38_hisat2 
  hisat2 -x hg38 -1 ${i} -2 ${fq2} -S ${sam} -p 20 
  
  echo "=======Bam works===========" 
- source /home/genwork1/anaconda3/etc/profile.d/conda.sh 
- conda activate sam 
  samtools view -@ 20 -b ${sam} | samtools sort -@ 20 -O bam -o ${sbam} 
  samtools index ${sbam} 
  rm ${sam} 
