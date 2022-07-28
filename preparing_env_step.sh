e35be4b42ad08d02566b2134f5aae4e81a956035
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-07-28 10:32:35 +0300

    Update preparing_env_step.sh

--- preparing_env_step.sh
+++ preparing_env_step.sh
@@ -3,3 +3,8 @@ conda create -n rnaseq_env
 conda install -c bioconda hisat2
 conda install -c bioconda samtools
 conda install -c bioconda subread
+mkdir refhg38
+cd refhg38
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.p13.genome.fa.gz
+gunzip GRCh38.p13.genome.fa.gz
+hisat2-build GRCh37.p13.genome.fa GRCh37.p13.genome.fa -p $1
