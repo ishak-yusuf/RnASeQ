f13181119abaa36b22bcb33c16080ec79f1c21aa
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-07-28 10:25:53 +0300

    Update preparing_env_step.sh

--- preparing_env_step.sh
+++ preparing_env_step.sh
@@ -1,4 +1,4 @@
-# conda is installed
+# The script is designed corresponding with ubuntu 22.04 and server already have conda environment.
 conda create -n rnaseq_env
 conda install -c bioconda hisat2
 conda install -c bioconda samtools
