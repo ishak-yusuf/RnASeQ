ec7f4e8f49ecb7154f34d8329bf0e721e6028766
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-09-30 11:07:31 +0300

    Update alignment_rate.py

--- rules/scripts/alignment_rate.py
+++ rules/scripts/alignment_rate.py
@@ -4,11 +4,11 @@ import os
 
 my_dict = {"Sample_Name":[],"Overall alignment rate":[]}
 for i in snakemake.input:
-    print(i)
+    sample= i.replace("step2/", "")
     lines = []
     with open(i) as f:
         lines = f.readlines()
-    my_dict["Sample_Name"].append(os.path.basename(i))
+    my_dict["Sample_Name"].append(sample)
     my_dict["Overall alignment rate"].append(re.search("\d+\s.\s\d+\smapped\s.(\d+.\d+.)", lines[4]).group(1))
 
 data = pd.DataFrame.from_dict(my_dict)
