6d99b0a5aebc35e1d1ac9b718e75bef3f95151ae
Ishak Yusuf <66043140+biocode-iy@users.noreply.github.com>
2022-09-30 11:07:34 +0300

    Update rnaseqc_sheet.py

--- rules/scripts/rnaseqc_sheet.py
+++ rules/scripts/rnaseqc_sheet.py
@@ -3,10 +3,12 @@ import pandas as pd
 my_dict = {"Sample_Name":[],"Mapped Reads":[],"Mapped Unique Reads":[],"Exonic Rate":[],"Intronic Rate":[],"Intergenic Rate":[],"Ambiguous Alignment Rate":[],"rRNA Rate Alignment":[]}
 
 for i in snakemake.input:
+    sample= i.replace("step2/", "").replace(".r", "")
+    filename= i + "/" +"{}.sorted.bam.metrics.tsv".format(sample)
     lines = []
-    with open(i) as f:
+    with open(filename) as f:
         lines = f.readlines()
-    my_dict["Sample_Name"].append(re.search("^Sample.(\w+.\d+.\w+).\w+.\w+.\w", lines[0]).group(1))
+    my_dict["Sample_Name"].append(sample)
     my_dict["Mapped Reads"].append(re.search("\w+\s\w+\t(\d+.\d+)", lines[51]).group(1))
     my_dict["Mapped Unique Reads"].append(re.search("\w+\s\w+\t(\d+.\d+)", lines[52]).group(1))
     my_dict["Exonic Rate"].append(re.search("\w+\s\w+\t(\d+|\d+.\d+)\n", lines[12]).group(1))
